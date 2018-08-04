205991d4095244c851b5aeabb9b42ac51893aea8
Hao-Yu Yang <deronmonta@gmail.com>
2018-08-03 17:09:35 -0700

    pytorch working

--- nets/nets.py
+++ nets/nets.py
@@ -4,23 +4,23 @@ import torch.utils as utils
 import torch.nn.init as init
 from torch.autograd import Variable
 import torch.nn.functional as F
-from layers import *
+from nets.layers import *
 
 class Dense_Net(nn.Module):
     def __init__(self, in_dim, out_dim, num_neurons):
         '''
         '''
-        print('\n---------------------Dense Net---------------------')
+        print('\n---------------------Dense Net Starting ---------------------')
         super(Dense_Net,self).__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.num_neurons = num_neurons
 
-        self.dense1 = dense_activation(in_dim,self.num_neurons*1)
+        self.dense1 = dense_activation(self.in_dim,self.num_neurons*1)
         self.dense2 = dense_activation(self.num_neurons*1,self.num_neurons*2)
         self.dense3 = dense_activation(self.num_neurons*2,self.num_neurons*4)
         self.dense4 = dense_activation(self.num_neurons*4,self.num_neurons*8)
-        self.dense5 = dense(self.num_neurons*8,self.output_dim)
+        self.dense5 = dense(self.num_neurons*8,self.out_dim)
 
     
     def forward(self,input):
